f506e31d8a3cb43aac7d463c8575c647cde39bbd
ChefSanto <114667038+ChefSanto@users.noreply.github.com>
2026-09-10 18:49:42 +0800

    Update test_navigation.py

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -1,24 +1,21 @@
 from playwright.sync_api import sync_playwright
 
 
-def test_mobile_navigation_inspection():
+def test_pandasail_navigation():
     with sync_playwright() as p:
         browser = p.chromium.launch()
-
-        page = browser.new_page(
-            viewport={"width": 390, "height": 844}
-        )
+        page = browser.new_page()
 
         page.goto("https://pandasail.com/")
 
-        print("\nMOBILE NAVIGATION ELEMENTS:")
+        desktop_menu = page.locator("#menu-1-da35919")
 
-        for nav in page.locator("nav").all():
-            print(
-                " - CLASS:",
-                nav.get_attribute("class"),
-                "| VISIBLE:",
-                nav.is_visible()
-            )
+        services_link = desktop_menu.get_by_role(
+            "link", name="Services"
+        )
+
+        services_link.click()
+
+        assert page.url == "https://pandasail.com/services/"
 
         browser.close()
